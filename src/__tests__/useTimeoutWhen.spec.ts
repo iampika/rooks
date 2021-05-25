23db91ca2758e66b582ca5dec5f3d6b5a05a83bc
Mahendra <pikaatic@gmail.com>
2021-05-25 15:48:40 +0530

    test: update tests

--- src/__tests__/useTimeoutWhen.spec.ts
+++ src/__tests__/useTimeoutWhen.spec.ts
@@ -24,6 +24,7 @@ describe('base', () => {
     };
   });
   it('runs immediately after mount', async () => {
+    expect.hasAssertions();
     jest.useFakeTimers();
     const { result } = renderHook(() => useHook());
     act(() => {
